75dd922dda158bd1dd86fa06316401f2ea4c44dc
SFA17237 <sebastien.farouz@open-groupe.com>
2020-01-21 11:53:38 +0100

    Add offers to project

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferBookingsPage implements OnInit {
 
-  constructor() { }
+  place: Place;
+
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
 
   ngOnInit() {
+      if (!this.route.snapshot.paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(this.route.snapshot.paramMap.get('placeId'));
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -36,5 +36,11 @@ export class PlacesService {
     return [...this._places];
   }
 
+  getPlace(id: string) {
+    return {...this.places.find(
+      p => p.id === id
+      )};
+  }
+
   constructor() { }
 }
