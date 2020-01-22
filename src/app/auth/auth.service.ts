b532f09f0719f99b443aff8eaf31927b5c43b4e4
SFA17237 <sebastien.farouz@open-groupe.com>
2020-01-22 11:19:17 +0100

    Add loading controller

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  private userIsAuthenticated = true;
+  private userIsAuthenticated = false;
 
   get IsAuthenticated() {
     return this.userIsAuthenticated;
