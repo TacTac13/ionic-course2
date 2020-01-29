50966608abd903654b76434ac0438c737ab04509
SFA17237 <sebastien.farouz@open-groupe.com>
2020-01-29 16:50:49 +0100

    fetching data

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -2,8 +2,56 @@ import { Injectable } from '@angular/core';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
 import { BehaviorSubject } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+
+// [
+//   new Place(
+//     'p1',
+//     'Manhattan Mansion',
+//     'In the heart of New York City',
+//     // tslint:disable-next-line: max-line-length
+// tslint:disable-next-line: max-line-length
+//     'https://patch.com/img/cdn20/users/22965231/20190709/105521/styles/patch_image/public/midtown-manhattan-new-york-city-new-york-shutterstock-412523491-1___09105440812.jpg',
+//     149.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'abc'
+//   ),
+//   new Place(
+//     'p2',
+//     ' L\'Amour Toujours',
+//     'A romantique place in Paris',
+//     // tslint:disable-next-line: max-line-length
+// tslint:disable-next-line: max-line-length
+//     'https://static.lexpress.fr/medias_11290/w_600,h_600,c_fill,g_north/v1484580707/france-paris-place-de-la-republique-square-renovated-in-2013-with-the-republic-monument-in-the-center_5780575.jpg',
+//     189.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'abc'
+//   ),
+//   new Place(
+//     'p3',
+//     'The Foggy Palace',
+//     'Not your average city trip!',
+//     'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg',
+//     99.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'abc'
+//   ),
+// ]
+
+
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -13,43 +61,36 @@ export class PlacesService {
   constructor(private authService: AuthService, private http: HttpClient) { }
 
   // tslint:disable-next-line: variable-name
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City',
-      // tslint:disable-next-line: max-line-length
-      'https://patch.com/img/cdn20/users/22965231/20190709/105521/styles/patch_image/public/midtown-manhattan-new-york-city-new-york-shutterstock-412523491-1___09105440812.jpg',
-      149.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      ' L\'Amour Toujours',
-      'A romantique place in Paris',
-      // tslint:disable-next-line: max-line-length
-      'https://static.lexpress.fr/medias_11290/w_600,h_600,c_fill,g_north/v1484580707/france-paris-place-de-la-republique-square-renovated-in-2013-with-the-republic-monument-in-the-center_5780575.jpg',
-      189.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip!',
-      'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg',
-      99.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
+  }
+
+  fetchPlace() {
+    return this.http.get<{ [key: string]: PlaceData }>('https://ionic-angular-course-f120c.firebaseio.com/offered-places.json')
+      .pipe(map(resData => {
+        const places = [];
+        for (const key in resData) {
+          if (resData.hasOwnProperty(key)) {
+            places.push(new Place(
+              key,
+              resData[key].title,
+              resData[key].description,
+              resData[key].imageUrl,
+              resData[key].price,
+              new Date(resData[key].availableFrom),
+              new Date(resData[key].availableTo),
+              resData[key].userId
+            ));
+          }
+        }
+        return places;
+      }),
+      tap(places => {
+        this._places.next(places);
+      })
+      );
   }
 
   getPlace(id: string) {
@@ -63,6 +104,7 @@ export class PlacesService {
   }
 
   addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+    let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -74,12 +116,20 @@ export class PlacesService {
       dateTo,
       this.authService.isUserId
     );
-    return this.http.post(
+    return this.http.post<{ name: string }>(
       'https://ionic-angular-course-f120c.firebaseio.com/offered-places.json',
-      {...newPlace, id: null})
-      .pipe(tap(resData => {
-        console.log(resData);
-      }));
+      { ...newPlace, id: null })
+      .pipe(
+        switchMap(resData => {
+          generatedId = resData.name;
+          return this.places;
+        }),
+        take(1),
+        tap(places => {
+          newPlace.id = generatedId;
+          this._places.next(places.concat(newPlace));
+        })
+      );
     // tslint:disable-next-line: align
     // return this.places.pipe(take(1), delay(1000), tap(places => {
     //   this._places.next(places.concat(newPlace));
@@ -88,23 +138,23 @@ export class PlacesService {
 
   updatePlace(placeId: string, title: string, description: string) {
     return this.places.pipe(take(1),
-    delay(1000),
-    tap(places => {
-      const udatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
-      const upatedPlaces = [...places];
-      const oldPlace = upatedPlaces[udatedPlaceIndex];
-      upatedPlaces[udatedPlaceIndex] = new Place(
-        oldPlace.id,
-        title,
-        description,
-        oldPlace.imageUrl,
-        oldPlace.price,
-        oldPlace.availableFrom,
-        oldPlace.availableTo,
-        oldPlace.userId
+      delay(1000),
+      tap(places => {
+        const udatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
+        const upatedPlaces = [...places];
+        const oldPlace = upatedPlaces[udatedPlaceIndex];
+        upatedPlaces[udatedPlaceIndex] = new Place(
+          oldPlace.id,
+          title,
+          description,
+          oldPlace.imageUrl,
+          oldPlace.price,
+          oldPlace.availableFrom,
+          oldPlace.availableTo,
+          oldPlace.userId
         );
-      this._places.next(upatedPlaces);
-    }));
+        this._places.next(upatedPlaces);
+      }));
   }
 
 }
